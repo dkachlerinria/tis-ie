a1b614ac59545e0c603bea3f30102fbf1f03c568
dkachlerinria <dimitri.kachler@inria.fr>
2026-05-09 23:32:09 +0200

    test

--- runs/config.sh
+++ runs/config.sh
@@ -3,7 +3,7 @@
 
 # General Config
 export BENCHMARK="bbh"
-export TRAINING_MODEL="Qwen/Qwen2.5-0.5B"
+export TRAINING_MODEL="Qwen/Qwen2.5-1.5B"
 export WARMUP_MODEL="${TRAINING_MODEL}"
 
 # Slug for file paths
