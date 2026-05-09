71d7bc468bc2b40d77241b61e670daa34c21c0df
dkachlerinria <dimitri.kachler@inria.fr>
2026-05-09 15:26:16 +0200

    test

--- runs/select_embedding.sh
+++ runs/select_embedding.sh
@@ -20,7 +20,7 @@ python3 -m representation.embed.compute_sentence_embeds \
     --dev_dataset_name "${BENCHMARK}" \
     --dev_index_path "${INDEX_DIR}/${BENCHMARK}_dev_embeds.pt" \
     --save_dir "${INDEX_DIR}" \
-    --batch_size 128 \
+    --batch_size 32 \
     --end_index "${END_INDEX}"
 
 # Step 2: Perform Data Selection
