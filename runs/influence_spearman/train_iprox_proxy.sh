b9592bcf5124097a688f9c6d29087b25315c4b9e
dkachlerinria <dimitri.kachler@inria.fr>
2026-05-15 17:45:24 +0200

    test

--- runs/influence_spearman/train_iprox_proxy.sh
+++ runs/influence_spearman/train_iprox_proxy.sh
@@ -20,11 +20,11 @@ python iprox/train_iprox.py \
     --train_dataset     "${TRAIN_DATASET}" \
     --n_train_p         8000 \
     --pool_start_index  "${END_INDEX}" \
-    --sparsity          0.8 \
-    --batch_size        1 \
+    --sparsity          0.9 \
+    --batch_size        2 \
     --gradient_accumulation_steps 1 \
     --lambda_anchor     0.0 \
-    --epochs            5 \
+    --epochs            1 \
     --max_seq_length    2048 \
     --output_dir        "${IPROX_PROXY_DIR}" \
     --lr                1e-4 \
