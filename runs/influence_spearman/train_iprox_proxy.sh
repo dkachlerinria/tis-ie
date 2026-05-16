70a7e674b372ffca9218f6638d00b6d37c6616ad
dkachlerinria <dimitri.kachler@inria.fr>
2026-05-16 16:41:26 +0200

    test

--- runs/influence_spearman/train_iprox_proxy.sh
+++ runs/influence_spearman/train_iprox_proxy.sh
@@ -24,7 +24,7 @@ python iprox/train_iprox.py \
     --batch_size        1 \
     --gradient_accumulation_steps 4 \
     --lambda_anchor     0.0 \
-    --epochs            20 \
+    --epochs            10 \
     --max_seq_length    2048 \
     --output_dir        "${IPROX_PROXY_DIR}" \
     --lr                1e-4 \
